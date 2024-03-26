0a224458e87b8526c02ae12d8db0d74e9c88a6cd
Joranvnp <joran.vanpeene@gmail.com>
2024-03-26 22:44:48 +0100

    Add eventListerner mouse, functions

--- src/components/magnetic/GsapMagnetic.jsx
+++ src/components/magnetic/GsapMagnetic.jsx
@@ -1,7 +1,23 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import gsap from "gsap";
 
 export const GsapMagnetic = ({ children }) => {
   const ref = useRef(null);
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      const { clientX, clientY } = e;
+    };
+
+    const mouseLeave = (e) => {};
+
+    ref.current.addEvenListener("mousemove", mouseMove);
+    ref.current.addEvenListener("mousemove", mouseLeave);
+
+    return () => {
+      ref.current.addEvenListener("mousemove", mouseMove);
+      ref.current.addEvenListener("mousemove", mouseLeave);
+    };
+  }, []);
   return <div ref={ref}>{children}</div>;
 };
